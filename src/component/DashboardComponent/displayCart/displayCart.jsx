5857321cbfd6047a08b0a2d95291e790fccb853b
md nahid <mdnahid360s@gmail.com>
2023-12-19 14:56:26 +0600

    update cart

--- src/component/DashboardComponent/displayCart/displayCart.jsx
+++ src/component/DashboardComponent/displayCart/displayCart.jsx
@@ -7,10 +7,10 @@ const DisplayCart = (props) => {
     const { theme } = useContext(AuthContext);
 
     return (
-        <div style={{ backgroundColor: bg }} className={` h-[155px] p-5 flex gap-4 justify-start items-center`}>
+        <div style={{ backgroundColor: bg }} className={` md:h-[155px] h-[130px] p-5 flex gap-4 justify-start items-center`}>
             {<img src={img} alt="icon" className="w-[80px]" />}
             <div className="">
-                {price && <h1 style={{ color: color }} className={`md:text-[1.70rem] text-4xl font-bold  `}>$ 5000</h1>}
+                {price && <h1 style={{ color: color }} className={`md:text-[1.70rem] text-[1.60rem] font-bold  `}>$ 5000</h1>}
                 <h5 className={`${theme ? 'text-white' : 'text-gray-700'} pt-2 pb-4`}>{subtitle}</h5>
                 {link && <Link href={link} className="px-6 py-1 rounded mt-6 text-white bg-blue-600">দেখুন</Link>}
             </div>
